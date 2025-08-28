f81731eba39e44edaa2d6b601c18e15df3229e75
userName <userEmail>
2025-08-28 17:35:48 +0600

    added code and modified

---
+++ src/app/middleware/validateRequest.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express"
+import { ZodObject } from "zod"
+
+
+export let validateRequest:any=(schema:ZodObject)=>{
+
+      return async(req:Request,res:Response,next:NextFunction)=>{
+    // // console.log(req.body.student)
+    // console.log(`hello hi ${hh}`)
+
+
+   try{
+     //validate check
+   await schema.parseAsync({
+    body:req.body
+   })
+
+
+     next()
+   }catch(err){
+    next(err)
+   }
+
+}
+
+}
--- src/app/modules/user/user/user.route.ts
+++ src/app/modules/user/user/user.route.ts
@@ -3,6 +3,7 @@ import express, { NextFunction, Request } from "express"
 import { createStudent } from "./user.controller"
 import { ZodObject } from "zod";
 import { studentZodSchema } from "../../student/validation.jod";
+import { validateRequest } from "../../../middleware/validateRequest";
 
 
 
@@ -12,28 +13,6 @@ import { studentZodSchema } from "../../student/validation.jod";
 
 const router= express.Router()
 
-let validateRequest:any=(schema:ZodObject)=>{
-
-      return async(req:Request,res:Response,next:NextFunction)=>{
-    // // console.log(req.body.student)
-    // console.log(`hello hi ${hh}`)
-
-
-   try{
-     //validate check
-   await schema.parseAsync({
-    body:req.body
-   })
-
-
-     next()
-   }catch(err){
-    next(err)
-   }
-
-}
-
-}
 
 
 // will call controller function
