dd5639de5e027006699b28d46bab75052caaa1bf
userName <userEmail>
2025-09-03 11:15:15 +0600

    academic department and faculty validated

--- src/app/modules/academicFaculty/academicFaculty.model.ts
+++ src/app/modules/academicFaculty/academicFaculty.model.ts
@@ -8,6 +8,37 @@ const academicFacultySchema = new Schema<TacademicFaculty>(
   { timestamps: true },
 )
 
+
+
+academicFacultySchema.pre("save",async function(next){
+
+     let isExist=await academicFacultyModel.findOne({name:this.name})
+
+    if(isExist){
+         throw new Error("Department Already Exists....n")
+    }
+    next()
+
+
+
+})
+
+
+academicFacultySchema.pre("updateOne",async function(next){
+
+    // console.log(this.getQuery()._id)
+     let payloadId=this.getQuery()._id
+    // let query=this.getQuery()
+    
+     let isExist=await academicFacultyModel.findOne({_id:payloadId})
+
+    if(!isExist){
+         throw new Error("this Department not Exists....nn")
+    }
+    next()
+
+})
+
 export const academicFacultyModel = model<TacademicFaculty>(
   'academicFaculty',
   academicFacultySchema,
